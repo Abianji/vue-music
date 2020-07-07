ffc6f67052e638a79e3dea555686d460a5d45264
DDXX <whynow@qq.com>
2020-07-07 19:26:35 +0800

    界面

---
+++ src/components/Player/PlayerTitleBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-system-bar window>
+    <v-menu offset-y transition="slide-y-transition">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn small right v-bind="attrs" v-on="on">
+          <v-icon>headset</v-icon>MENU
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item @click="dialog = true">
+          <v-list-item-title>About</v-list-item-title>
+        </v-list-item>
+      </v-list>
+
+      <v-dialog v-model="dialog" max-width="300">
+        <v-card>
+          <v-card-title>
+            <h2>Vue Music Player</h2>
+          </v-card-title>
+          <v-card-text>Version 1.0.0</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="dialog = false">OK</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-menu>
+    <v-spacer></v-spacer>VUE MUSIC PLAYER
+    <v-spacer></v-spacer>
+    <v-icon>remove</v-icon>
+    <v-icon>check_box_outline_blank</v-icon>
+    <v-icon>close</v-icon>
+  </v-system-bar>
+</template>
+
+
+<script>
+export default {
+  data: () => ({
+    dialog: false
+  }),
+  methods: {
+    method() {
+      // Perform an action
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Player.vue
@@ -0,0 +1,15 @@
+<template>
+  <PlayerTitleBar></PlayerTitleBar>
+</template>
+
+<script>
+import PlayerTitleBar from "../components/Player/PlayerTitleBar";
+export default {
+  components: {
+    PlayerTitleBar
+  }
+};
+</script>
+
+<style>
+</style>
